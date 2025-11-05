f8743e40287ab4f2d6e4b15cce2994336daf6846
lionsunday <hamakoyamak@gmail.com>
2025-11-05 09:26:18 +0800

    home page initial

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import { FHERaceTrackDemo } from "@/components/FHERaceTrackDemo";
+
+export default function Home() {
+  return (
+    <main className="w-full">
+      <div className="flex flex-col gap-8 items-center w-full">
+        <FHERaceTrackDemo />
+      </div>
+    </main>
+  );
+}
+
